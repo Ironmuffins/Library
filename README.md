5c454d25ccea66b10295289bd04ac547a892ae66
Jared <midnightfuture@protonmail.com>
2021-02-18 10:57:42 -0800

    Created basic HTML form so user can input data.

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 New features:
-Initial commit.
+User able to input book info.
+Created basic HTML form.
 
 Bug fixes:
 None.
 
 Current features:
-None.
+User can input info into form.
 
 Planned features:
+Book info is placed into object.
+book object in placed into library.
+User can select any item in library and see the info.
+Make it pretty.
--- library.js
+++ library.js
@@ -1,13 +1,21 @@
-function books(name, author, readstatus) {
+let myLibrary = [];
+
+function Book(name, author, pages, readstatus) {
     this.name = name
     this.author = author
+    this.pages = pages
     this.readstatus = readstatus
     this.bookinfo = function() {
-      return(name + " by " + author + ". I have " + readstatus)
+     
     }
-  }
+  };
 
-  const theHobbit = new books('The Hobbit', 'J.R.R. Tolkein', 'read it twice');
-  const HarryPotter = new books('Harry Potter', 'J.K Rowling', "never read it");
-  theHobbit.bookinfo();
-  console.log(theHobbit.bookinfo());
+function addBookToLibrary() {
+  //const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkein', '325', 'read it twice');
+  //push
+}
+
+  
+  //const HarryPotter = new Book('Harry Potter', 'J.K Rowling', "never read it");
+
+  //bookname = document.querySelector('#bookName')
