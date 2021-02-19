2872ee044cdc898bf59e2b6c1aa421105e7447a1
Jared <midnightfuture@protonmail.com>
2021-02-18 20:54:05 -0800

    Added function for displaying library books. Removed unnecssary global variables.

--- library.js
+++ library.js
@@ -1,29 +1,43 @@
-let myLibrary = [];
-let bookName;
-let author;
-let pages;
-let readStatus;
+//Initial variables
+let book1 = new Book('LOTR', 'J.R.R tolkein', '1000', 'yes');
+let book2 = new Book('Homo Deus', 'Yuval Noah Harari', '575', 'yes');
+let book3 = new Book('Pride and Prejudice', 'Somebody?', '750', 'no');
+let myLibrary = [book1, book2, book3];
 
+//Object constructor/Blueprint
 function Book(bookName, author, pages, readStatus) {
     this.bookNname = bookName
     this.author = author
     this.pages = pages
     this.readStatus = readStatus
-    this.bookinfo = function() {
-
-    }
+//  this.bookinfo = function() {
+//
+//  }
   };
 
-addBook = document.querySelector('#addBook');
-addBook.addEventListener('click', addBookToLibrary)
-
+//Add Book button
+const addBook = document.querySelector('#addBook');
+addBook.addEventListener('click', addBookToLibrary);
 function addBookToLibrary() {
-  bookName = document.querySelector('#bookName').value;
-  author = document.querySelector('#author').value;
-  pages = document.querySelector('#pages').value;
-  readStatus = document.querySelector('#readStatus').value;
-  let newBook = new Book(bookName, author, pages, readStatus)
-  console.log(newBook);
+  newBookName = document.querySelector('#bookName').value;
+  newAuthor = document.querySelector('#author').value;
+  newPages = document.querySelector('#pages').value;
+  newReadStatus = document.querySelector('#readStatus').value;
+  let newBook = new Book(newBookName, newAuthor, newPages, newReadStatus);
   myLibrary.push(newBook);
   console.log(myLibrary);
+};
+
+//Display Library
+const openLibrary = document.querySelector('#openLibrary');
+openLibrary.addEventListener('click', libraryDisplay);
+const libraryDisplayDiv = document.querySelector('.libraryDisplayDiv');
+function libraryDisplay() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    let bookItemDiv = document.createElement("div");
+    bookItemDiv.className = 'bookItemDiv';
+    bookItemDiv.id = "bookItemDiv" + [i];
+    libraryDisplayDiv.appendChild(bookItemDiv);
+    console.log('iterations');
+}
 }
